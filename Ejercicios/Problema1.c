dbcbf2cf5eba00529153af92743d7a0059d6758b
rodrigo <rrcc_999@hotmail.com>
2021-05-05 21:09:26 -0600

    listo ejercicio 1

--- Ejercicios/Problema1.c
+++ Ejercicios/Problema1.c
@@ -11,29 +11,36 @@ Salida: Vector ordenado
 
 //Librerias
 #include <stdio.h>
-//pasos del pseudocódigo
 
 void main(void){
+    //usamos un while para que el programa sea utilizado las veces que el usuario lo desee
     while (1){
+        //definimos a x como la variable que almacena el modo de desplegar los vectores
         char x;
+        //definimos el vector a mostrar
         int a[10]={2,4,6,8,10,12,14,16,18,20};
+        //pedimos que el usuario ingrese el modo de desplegue
         printf("Ingrese a si desea ver el vector en forma ascendente y d en forma descendente. Presione cualquier otro caracter para salir. \n");
         scanf("%s",&x);
+        //utilizamos un switch para que alterne entre el estado ascendente, a, y el descendente, d.
         switch (x)
         {
         case 'a':
             for (int i = 0; i < 10; i++)
             {
+                //desplegamos el vector ascendentemente con un for que va de 0 a 9
                 printf("%d \n",a[i]);
             }
             break;
         case 'd':
             for (int i = 9; i >= 0; i--)
             {
+                //desplegamos el vector descendentemente con un for en forma regresiva, de 9 a 0
                 printf("%d \n",a[i]);
             }
             break;
         default:
+            //en caso el usuario no ingrese 'a' ni 'd', el programa termina
             return;
             break;
         }
